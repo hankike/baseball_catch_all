129a5539e445b8cf56f64f4124489baa655a19b6
Hank Eichstaedt <128631771+hankike@users.noreply.github.com>
2026-04-24 00:23:20 -0400

    Update speed_slug.R

--- speed_slug.R
+++ speed_slug.R
@@ -1,34 +1,31 @@
 library(dplyr)
 library(ggplot2)
 
-# Need to download the data set online. It downloads as savant_data. will be changing name to hoerner_2025
+# Downloading two sets for my comparison
+# Need to download the data set online. It downloads as savant_data. will be changing name to hoerner_2025 and schwarber_2025
 
 hoerner_2025 <- read.csv("C:/Users/hanke/Downloads/hoerner_2025.csv")
+schwarber_2025 <- read.csv("C:/Users/hanke/Downloads/schwarber_2025.csv")
 
 # There is a lot of data here. I will condense it
 columns = c("pitch_type","pitch_name", "release_speed", "events", "bb_type", "hc_x", "hc_y",
             "hit_distance_sc", "launch_speed", "launch_angle", "release_pos_y",
             "bat_speed", "swing_length")
 
-test = hoerner_2025[, columns]
-
-# Lets see how many of each event we have
-count(test, events, sort = TRUE)
-
-nrow(test[test$events == "double" | test$events == "triple" , ])
-
-nrow(test[test$events == "double" & test$hit_distance_sc < 200
-          | test$events == "triple" & test$hit_distance_sc < 200, ])
-
-nrow(test[test$events == "single" & test$hit_distance_sc > 200, ])
-
-
-
-
-schwarber_2025 <- read.csv("C:/Users/hanke/Downloads/schwarber_2025.csv")
-
+hoerner_2025 = hoerner_2025[, columns]
 schwarber_2025 = schwarber_2025[, columns]
 
+# Hoerner
+count(hoerner_2025, events, sort = TRUE)
+
+nrow(hoerner_2025[hoerner_2025$events == "double" | hoerner_2025$events == "triple" , ])
+
+nrow(hoerner_2025[hoerner_2025$events == "double" & hoerner_2025$hit_distance_sc < 200
+          | hoerner_2025$events == "triple" & hoerner_2025$hit_distance_sc < 200, ])
+
+nrow(hoerner_2025[hoerner_2025$events == "single" & hoerner_2025$hit_distance_sc > 200, ])
+
+# Schwarber
 count(schwarber_2025, events, sort = TRUE)
 
 nrow(schwarber_2025[schwarber_2025$events == "double" | schwarber_2025$events == "triple" , ])
